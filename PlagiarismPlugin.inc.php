e37b282a1abf8c3b0c67a9957ee52e78a43a7704
Alec Smecher <alec@smecher.bc.ca>
2019-03-04 17:00:15 -0800

    Fix warning

--- PlagiarismPlugin.inc.php
+++ PlagiarismPlugin.inc.php
@@ -16,8 +16,8 @@ class PlagiarismPlugin extends GenericPlugin {
 	/**
 	 * @copydoc Plugin::register()
 	 */
-	public function register($category, $path) {
-		$success = parent::register($category, $path);
+	public function register($category, $path, $mainContextId = null) {
+		$success = parent::register($category, $path, $mainContextId);
 		$this->addLocaleData();
 
 		if ($success && Config::getVar('ithenticate', 'ithenticate') && $this->getEnabled()) {
