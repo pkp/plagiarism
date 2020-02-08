a5281f2fe45e686e7ad80d0ef616501b8a8dae69
Alec Smecher <alec@smecher.bc.ca>
2020-02-07 17:06:18 -0800

    pkp/pkp-lib#5488 Update copyright dates and GPLv2 license to GPLv3

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
 /**
  * @file plugins/generic/plagiarism/index.php
  *
- * Copyright (c) 2003-2018 Simon Fraser University
- * Copyright (c) 2003-2018 John Willinsky
- * Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+ * Copyright (c) 2003-2020 Simon Fraser University
+ * Copyright (c) 2003-2020 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @ingroup plugins_generic_plagiarism
  * @brief Wrapper for plagiarism checking plugin.
