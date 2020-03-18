3d668875e850040157ae419b2d955f2f7f62d5f4
Alec Smecher <alec@smecher.bc.ca>
2020-03-18 16:55:50 -0700

    Version number updates

--- index.php
+++ index.php
@@ -1,20 +1,13 @@
 <?php
 
 /**
- * @defgroup plugins_generic_plagiarism
- */
-
-/**
  * @file plugins/generic/plagiarism/index.php
  *
  * Copyright (c) 2003-2020 Simon Fraser University
  * Copyright (c) 2003-2020 John Willinsky
- * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
+ * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
  *
- * @ingroup plugins_generic_plagiarism
  * @brief Wrapper for plagiarism checking plugin.
- *
  */
 require_once('PlagiarismPlugin.inc.php');
-
 return new PlagiarismPlugin();
