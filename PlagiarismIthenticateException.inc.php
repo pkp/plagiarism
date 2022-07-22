9e5c4de386015fd902b2c6de5f42c5ef67ac9d1f
Clinton Graham <ctgraham@pitt.edu>
2022-07-22 09:37:00 -0400

    #23, #28 Add custom exception for ithenticate errors

---
+++ PlagiarismIthenticateException.inc.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @file PlagiarismIthenticateException.inc.php
+ *
+ * Copyright (c) 2003-2021 Simon Fraser University
+ * Copyright (c) 2003-2021 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
+ *
+ * @brief Plagiarism plugin iThenticate exception
+ */
+
+
+/**
+ * Define an exception class for iThenticate via this plugin
+ */
+class PlagiarismIthenticateException extends Exception
+{
+	private ?\bsobbe\ithenticate\Ithenticate $connection;
+
+	/*
+	 * Constructor
+	 * @param $message string
+	 * @param $code int
+	 * @param $previous Exception
+	 * @param $connection Ithenticate
+	 */
+	public function __construct($message, $code = 0, ?Throwable $previous = null, ?\bsobbe\ithenticate\Ithenticate $connection = null) {
+		$this->connection = $connection;
+		parent::__construct($message, $code, $previous);
+	}
+
+	/*
+	 * Constructor
+	 * @return Ithenticate|null
+	 */
+	public function getConnection() {
+		return $this->connection;
+	}
+}
+
