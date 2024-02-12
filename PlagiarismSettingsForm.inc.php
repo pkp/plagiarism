ae26048ff1bd92ed6ad4f840b9d62fdc5657d56f
Touhidur Rahman <abir.ruet@gmail.com>
2024-02-12 12:41:08 +0600

    pkp/plagiarism#52 added class doc block for Plagiarism Settings Form class

--- PlagiarismSettingsForm.inc.php
+++ PlagiarismSettingsForm.inc.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * @file PlagiarismSettingsForm.inc.php
+ *
+ * Copyright (c) 2003-2024 Simon Fraser University
+ * Copyright (c) 2003-2024 John Willinsky
+ * Distributed under the GNU GPL v3. For full terms see the file LICENSE.
+ *
+ * @brief Plagiarism settings form
+ */
+
 import('lib.pkp.classes.form.Form');
 import('plugins.generic.plagiarism.PlagiarismPlugin');
 
